a93c147ab4315a01e1accb88fc27e1d51e5576f7
James Crockett <jecrockett@gmail.com>
2016-02-16 16:18:41 -0700

    Complete Twitter login through Oauth

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+
+  def self.find_or_create_from_auth(auth)
+    user = User.find_or_create_by(uid: auth['uid'], provider: auth['provider'])
+
+    user.nickname = auth['info']['nickname']
+    user.name = auth['info']['name']
+    user.email = auth['info']['email']
+    user.image_url = auth['info']['image']
+    user.token = auth['credentials']['token']
+
+    user.save
+    user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  root "home#show"
+  get "dashboard", to: "users#show"
+  get "auth/twitter", as: :twitter_login
+  get "auth/:provider/callback", to: "sessions#create"
+
 end
